b56355c74463fc32e27d9d82bc67f8dbde5e90e1
iain-code <iainvirgo1010@live.co.uk>
2025-03-03 09:15:00 +0200

    1

--- Dockerfile
+++ Dockerfile
@@ -2,4 +2,5 @@ FROM debian:stable-slim
 # COPY source destination
 COPY dockerized /bin/dockerized
 
+ENV PORT=8080
 CMD ["/bin/dockerized"]
--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"time"
 )
 
@@ -12,17 +13,20 @@ func main() {
 
 	m.HandleFunc("/", handlePage)
 
-	const addr = ":8080"
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080" // default if not set
+	}
 	srv := http.Server{
 		Handler:      m,
-		Addr:         addr,
+		Addr:         ":" + port, // This is important!
 		WriteTimeout: 30 * time.Second,
 		ReadTimeout:  30 * time.Second,
 	}
 
 	// this blocks forever, until the server
 	// has an unrecoverable error
-	fmt.Println("server started on ", addr)
+	fmt.Println("server started on :" + port)
 	err := srv.ListenAndServe()
 	log.Fatal(err)
 }
@@ -33,7 +37,7 @@ func handlePage(w http.ResponseWriter, r *http.Request) {
 	const page = `<html>
 <head></head>
 <body>
-	<p> Hello from Docker! I'm a Go server. </p>
+	<p> This is Iains new version and its the best version! </p>
 </body>
 </html>
 `
